a4744e646eba9898c47b92ebcfb860294b5c17c4
Neto test <nikolajeliseev834@gmail.com>
2025-05-09 19:43:15 +0300

    Переработка логики

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { MessageHistory } from './components/MessageHistory';
+import { messages } from './messagesData/messages'; 
 import './index.css';
 
 function App() {
-  return <>test</>;
+  return (
+    <div className="clearfix container">
+      <div className="chat">
+        <div className="chat-history">
+          <MessageHistory list={messages} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
